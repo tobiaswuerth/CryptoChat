41c6bac8c15ac6bd7bb030d5be6feeded0e5c877
UpZone <mail@up.zone>
2017-05-15 20:26:06 +0200

    updated vendor scripts and cleaned up

--- CryptoChat/scripts/vendor/security/xss.js
+++ CryptoChat/scripts/vendor/security/xss.js
@@ -1,3 +1,5 @@
+// src https://github.com/leizongmin/js-xss
+
 (function e(t, n, r) {
     function s(o, u) {
         if (!n[o]) {
@@ -10,29 +12,29 @@
                     return i(o, !0);
                 }
                 const f = new Error(`Cannot find module '${o}'`);
-                throw f.code = "MODULE_NOT_FOUND", f
+                throw f.code = "MODULE_NOT_FOUND", f;
             }
             const l = n[o] = { exports: {} };
             t[o][0].call(l.exports,
                 function(e) {
                     const n = t[o][1][e];
-                    return s(n ? n : e)
+                    return s(n ? n : e);
                 },
                 l,
                 l.exports,
                 e,
                 t,
                 n,
-                r)
+                r);
         }
-        return n[o].exports
+        return n[o].exports;
     }
 
     var i = typeof require == "function" && require;
     for (let o = 0; o < r.length; o++) {
         s(r[o]);
     }
-    return s
+    return s;
 })({
         1: [
             function(require, module, exports) {
